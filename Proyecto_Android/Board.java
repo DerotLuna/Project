fd9c9c184ffa18a73b240ce0fe1b908c924cb3e4
morty <alemv96@hotmail.com>
2016-05-15 18:57:12 -0430

    Update Board.java

--- Proyecto_Android/Board.java
+++ Proyecto_Android/Board.java
@@ -17,7 +17,5 @@ public class Board {
 }
 /*recordatorio: necesito la relacion entre casillas para la vecindad.
 creo que si va a ser necesario crear un tablero que extienda de board (ruben lo dijo);
-Idea: board solo se encargaria la condicion en comun que deben de cumplir todos los tableros
-bien sean rectangulares, sean triangulares o de cualquier forma es decir, verificar las filas, columnas y
-crear las capas necesarias para esto*/
+World es quien se encargara de posicionar las pieza en el tablero*/
 
