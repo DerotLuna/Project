c8aa5f56a099dbed0590d9ef5ded1e97f20f802e
morty <alemv96@hotmail.com>
2016-05-15 16:01:46 -0430

    Update Board.java

--- Proyecto_Android/Board.java
+++ Proyecto_Android/Board.java
@@ -1,3 +1,23 @@
-public class Board {
-  
+ic abstract class Board {
+
+  private String shape;
+  private Layer[] layer;
+
+  public Board(String shape){
+    this.shape = shape;
+
+  }
+
+  public String getShape(){
+    return this.shape;
+  }
+
+  public abstract int getIdBox(int positionY , int positionX);
+
 }
+/*recordatorio: necesito la relacion entre casillas para la vecindad.
+creo que si va a ser necesario crear un tablero que extienda de board (ruben lo dijo);
+Idea: board solo se encargaria la condicion en comun que deben de cumplir todos los tableros
+bien sean rectangulares, sean triangulares o de cualquier forma es decir, verificar las filas, columnas y
+crear las capas necesarias para esto*/
+
