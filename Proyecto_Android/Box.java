3930ef04c86c70c2f1c046e62083937342fe7110
morty <alemv96@hotmail.com>
2016-05-15 17:14:08 -0430

    Update Box.java

--- Proyecto_Android/Box.java
+++ Proyecto_Android/Box.java
@@ -1,5 +1,24 @@
 public class Box {
   //atributos
   private int id;
-  
+  private int boxPosition;
+  private Status statusBox;
+
+
+  public Box(int boxPosition,Status statusBox){
+    this.boxPosition = boxPosition;
+    this.statusBox = statusBox;
+    this.id =//pensar la manera de conseguir el id de una casilla.
+  }
+
+  public void getId(){
+    return this.id;
+  }
+
+  public Status getStatus(){
+    return this.statusBox;
+  }
+
+
+
 }
