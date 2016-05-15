b9a53131e8fd25ecf197802b904e1f944013a125
morty <alemv96@hotmail.com>
2016-05-15 15:58:59 -0430

    Update Square_Board.java

--- Proyecto_Android/Square_Board.java
+++ Proyecto_Android/Square_Board.java
@@ -13,14 +13,15 @@ public class Square_Board extends Board{
 
     for (int numberBox = 0; countBoxes < dimension ; countBoxes++){
     //aca se crean las casillas del tablero.
-      boxes[numberBox] = new Box(numberBox , statusBox.EMPTY);//cuando se crean por ahora se colocaran libres.
+      boxes[numberBox] = new Box(numberBox , this.statusBox.EMPTY);//cuando se crean por ahora se colocaran libres.
     }
 
    //no estoy seguro de que esta matriz sea de esta forma
   /*for (int row = 0; row < dimension ; row++){ matriz de Adyacencias
       for (int column = 0; column <dimension ; column++){
-          this.matrixAdjacency[row][column] = false;
-      }
+         this. matrixAdjacency[row][column] = false;
+                      **CODIGO**
+      } 
     }*/
 
   }
