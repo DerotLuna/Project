ba86f8fe2ab68eb65ec227da971c341654aec993
morty <alemv96@hotmail.com>
2016-06-08 10:55:53 -0430

    Add files via upload

---
+++ Final-Project/Status.java
@@ -0,0 +1,10 @@
+public interface Status{
+
+  public String getStatusBox();
+}
+
+/*
+  Esto puede ser una interface, se considerara dependiendo
+  de si necesitaremos atributos o solo la creacion de metodos
+  con solo su "firmas".
+*/
